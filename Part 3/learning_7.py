aa841e71e45a987eecf08abf9ac144fe81c91d6e
HANYIIK <295928517@qq.com>
2020-02-22 18:25:07 +0800

    test 2

--- Part 3/learning_7.py
+++ Part 3/learning_7.py
@@ -1,2 +1,12 @@
 # Chapter 19
 import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# 画图函数
+def DrawImage(images_set, titles_set, rows, cols, num):
+    for i in range(num):
+        plt.subplot(rows, cols, i+1), plt.imshow(images_set[i]), plt.title(titles_set[i])
+        plt.xticks([]), plt.yticks([])
+    plt.show()
