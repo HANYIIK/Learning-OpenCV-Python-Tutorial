c69c7fc978076cf584074637afe523b352bdb773
HANYIIK <295928517@qq.com>
2020-02-23 18:42:34 +0800

    2020.02.23 18:42

--- Part 3/learning_7.py
+++ Part 3/learning_7.py
@@ -1,12 +1,13 @@
-# Chapter 19
+# Chapter 19 Canny 边缘检测
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 
 # 画图函数
-def DrawImage(images_set, titles_set, rows, cols, num):
+def DrawPictures(images_set, titles_set, rows, cols, num):
     for i in range(num):
         plt.subplot(rows, cols, i+1), plt.imshow(images_set[i]), plt.title(titles_set[i])
         plt.xticks([]), plt.yticks([])
     plt.show()
+
