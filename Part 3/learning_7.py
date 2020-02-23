67f669cb48d99cef917681ca863c4015c00f24a7
HANYIIK <295928517@qq.com>
2020-02-23 21:33:22 +0800

    2020.02.23 21:33

--- Part 3/learning_7.py
+++ Part 3/learning_7.py
@@ -1,4 +1,8 @@
 # Chapter 19 Canny 边缘检测
+"""
+Canny边缘检测:
+cv2.Canny(img, 最低阈值, 最高阈值)
+"""
 import cv2
 from matplotlib import pyplot as plt
 
@@ -31,6 +35,14 @@ def ShowPictures(images_set_2, titles_set_2, num_2, rate_2):
 # 画图集函数
 def DrawPictures(images_set, titles_set, rows, cols, num):
     for i in range(num):
-        plt.subplot(rows, cols, i+1), plt.imshow(images_set[i]), plt.title(titles_set[i])
+        plt.subplot(rows, cols, i+1), plt.imshow(images_set[i], cmap='gray'), plt.title(titles_set[i])
         plt.xticks([]), plt.yticks([])
     plt.show()
+
+img = cv2.imread('13.png', 0)
+edges = cv2.Canny(img, 30, 70)
+
+images = [img, edges]
+titles = ['Original', 'Canny']
+
+DrawPictures(images, titles, 1, 2, 2)
