3fda7ccd8c45cbd4ebc9c615ab6a28dc15ce2fe6
HANYIIK <295928517@qq.com>
2020-02-26 03:11:02 +0800

    2020.02.25 3:10

--- Part 3/learning_8.py
+++ Part 3/learning_8.py
@@ -1,7 +1,14 @@
-# Chapter 20 图像金字塔
+# Chapter 20 图像金字塔与图像融合
+"""
+【同一图像】【不同分辨率】的子图集合
+分辨率最【大】的图像放在【底】部, 分辨率最【小】的放在【􏰐顶】部􏰑􏰐
+分类：
+􏰓① 高斯金字塔: 顶部是通过􏰑􏰐􏰔􏰕将底部􏰐图像中的􏰖连续的􏰉行和列去除􏰗得到的
+② 拉普拉斯金字塔
+"""
 import cv2
-import numpy as np
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 # 保存图像函数
@@ -10,7 +17,7 @@ def SaveOrNot(pic):
         print('Not saved!')
         cv2.destroyAllWindows()
     elif cv2.waitKey(0) == ord('s'):  # wait for 's' to save and exit
-        cv2.imwrite('result.jpg', pic)  # save
+        cv2.imwrite('Orange.jpg', pic)  # save
         print('Saved successfully!')
         cv2.destroyAllWindows()
 
@@ -36,3 +43,46 @@ def DrawPictures(images_set, titles_set, rows, cols, num):
         plt.xticks([]), plt.yticks([])
     plt.show()
 
+apple_1 = cv2.imread('apple.png')
+apple = cv2.resize(apple_1, (256, 256))
+orange_1 = cv2.imread('Orange.jpg')
+orange = cv2.resize(orange_1, (256, 256))
+
+# 生成 apple 的高斯金字塔
+gaussian = apple.copy()
+group_gaussian_apple = [gaussian]
+for i in range(6):
+    gaussian = cv2.pyrDown(gaussian)
+    group_gaussian_apple.append(gaussian)
+
+# 生成 orange 的高斯金字塔
+gaussian = orange.copy()
+group_gaussian_orange = [gaussian]
+for i in range(6):
+    gaussian = cv2.pyrDown(gaussian)
+    group_gaussian_orange.append(gaussian)
+
+# 生成 apple 的拉普拉斯金字塔
+group_laplacian_apple = [group_gaussian_apple[5]]
+for i in range(5, 0, -1):
+    laplacian = cv2.pyrUp(group_gaussian_apple[i])
+    group_laplacian_apple.append(laplacian)
+
+# 生成 orange 的拉普拉斯金字塔
+group_laplacian_orange = [group_gaussian_orange[5]]
+for i in range(5, 0, -1):
+    laplacian = cv2.pyrUp(group_gaussian_orange[i])
+    group_laplacian_orange.append(laplacian)
+
+LS = []
+for la, lo in zip(group_laplacian_apple, group_laplacian_orange):
+    rows, cols, dpt = la.shape
+    ls = np.hstack((la[:, 0:int(cols / 2)], lo[:, int(cols/2):]))
+    LS.append(ls)
+
+ls_ = LS[0]
+for i in range(1, 6):
+    ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.add(ls_, LS[i])
+
+ShowImage('test', ls_, 1)
