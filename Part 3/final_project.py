44a151ac8acf563fc47e0f93eafcad4ccceba806
HANYIIK <295928517@qq.com>
2020-03-24 18:56:14 +0800

    2020.03.24 18:56

---
+++ Part 3/final_project.py
@@ -0,0 +1,20 @@
+# 车道检测
+import cv2
+import numpy as np
+
+
+# 显示图像函数
+def ShowImage(name_of_image, image, rate):
+    img_min = cv2.resize(image, None, fx=rate, fy=rate, interpolation=cv2.INTER_CUBIC)
+    cv2.namedWindow(name_of_image, cv2.WINDOW_NORMAL)
+    cv2.imshow(name_of_image, img_min)
+    if cv2.waitKey(0) == 27:  # wait for ESC to exit
+        print('Not saved!')
+        cv2.destroyAllWindows()
+    elif cv2.waitKey(0) == ord('s'):  # wait for 's' to save and exit
+        cv2.imwrite(name_of_image + '.jpg', image)  # save
+        print('Saved successfully!')
+        cv2.destroyAllWindows()
+
+img = cv2.imread('lane.jpg')
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
